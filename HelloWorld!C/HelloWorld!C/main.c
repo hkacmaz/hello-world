991b2051cd94c7b835d8160f4ae5cc93c6493a43
hkacmaz <halilkacmaz@outlook.com>
2019-02-15 23:31:19 -0800

    HelloWorld! in C :)

---
+++ HelloWorld!C/HelloWorld!C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld!C
+//
+//  Created by Halil Kacmaz on 2/15/19.
+//  Copyright © 2019 Halil Kacmaz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
